44ba83c5fba1668a847e143923bddfffc1f267d9
davincikab <davidnganganjeri079@gmail.com>
2020-12-23 09:04:51 +0300

    apartment form

--- property/forms.py
+++ property/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import Property
+from .models import Property, Apartment, Tenants, RentPayment
 
 class PropertyForm(forms.ModelForm):
     class Meta:
         model = Property
         exclude = ('slug',)
+
+class ApartmentForm(forms.ModelForm):
+    
+    class Meta:
+        model = Apartment
+        fields = "__all__"
+
